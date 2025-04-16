e3fd1f8cafa4b5c2e120666da0fc1cbcd483ea99
tejaswi-cm <pohankartejaswini3@gmail.com>
2025-04-16 14:35:28 -0700

    first commit

--- src/main/java/com/tka/sams/api/SamsTrackApplication.java
+++ src/main/java/com/tka/sams/api/SamsTrackApplication.java
@@ -8,7 +8,7 @@ public class SamsTrackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamsTrackApplication.class, args);
-		System.out.println("Started....");
+		System.out.println("Started Application....");
 	}
 
 }
